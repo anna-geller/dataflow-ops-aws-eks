97b59e06cdfddd2544a919f2ade132d7c062d8d0
Anna Geller <anna@prefect.io>
2022-09-07 14:11:47 +0200

    py 3.10

--- blocks/kubernetes-job/image_pull_policy.py
+++ blocks/kubernetes-job/image_pull_policy.py
@@ -2,7 +2,7 @@ from prefect.infrastructure import KubernetesJob, KubernetesImagePullPolicy
 
 k8s_job = KubernetesJob(
     namespace="prod",
-    image="prefecthq/prefect:2-python3.9",
+    image="prefecthq/prefect:2-python3.10",
     image_pull_policy=KubernetesImagePullPolicy.IF_NOT_PRESENT,
     env={"EXTRA_PIP_PACKAGES": "s3fs", "PREFECT_LOGGING_LEVEL": "DEBUG"},
 )
