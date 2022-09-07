fba7abc159f48836dce9799690346b3cf30beed4
Anna Geller <anna@prefect.io>
2022-09-07 14:26:24 +0200

    back to python 3.9

--- blocks/k8s.py
+++ blocks/k8s.py
@@ -1,7 +1,7 @@
 from prefect.infrastructure import KubernetesJob
 
 k8s = KubernetesJob(
-    image="prefecthq/prefect:2-python3.10",
+    image="prefecthq/prefect:2-python3.9",
     namespace="prod",
     image_pull_policy="IfNotPresent",
     env={"PREFECT_LOGGING_LEVEL": "DEBUG"},
